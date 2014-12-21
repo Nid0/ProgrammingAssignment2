a3379a6cdee353f3981215349862141a78cc6cf0
nid0 <n.ababtain@gmail.com>
2014-12-21 16:28:42 +0300

    commit1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Mession: Write a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## "makeCacheMatrix" creates a special "matrix" object
+## that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse<- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+##"cacheSolve" computes the inverse of the special "matrix" returned by "makeCacheMatrix" 
+##above. And calculates the inverse if it has not already been calculated. And if the 
+##matrix has not changed then"cacheSolve" should retrieve the inverse from the cache.
 
+}
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached inverse of matrix")
+                return(m)
+        } else {
+                m <- solve(x$get())
+                x$setinverse(m)
+                m 
+        }              
 }
